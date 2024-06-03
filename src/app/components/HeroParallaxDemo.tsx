9d1fbc3d201451b1273b8efe2fc390f0b9327875
Anmol <anmolawasthi117@gmail.com>
2024-06-04 00:53:24 +0530

    removed use client form page.tsx

--- src/app/components/HeroParallaxDemo.tsx
+++ src/app/components/HeroParallaxDemo.tsx
@@ -1,9 +1,16 @@
 "use client";
 import React from "react";
 import { HeroParallax } from "./ui/hero-parallax";
-
+import { BrowserRouter as Router } from 'react-router-dom';
 export function HeroParallaxDemo() {
-  return <HeroParallax products={products} />;
+  return(
+    <div>
+      <Router>
+      <HeroParallax products={products} />
+      </Router>
+
+    </div> 
+  );
 }
 export const products = [
   {
@@ -27,7 +34,7 @@ export const products = [
 
   {
     title: "img 4",
-    link: "https://editorially.org",
+    link: "#",
     thumbnail:
       "https://www.ineffa.in/_next/static/media/Image_4.0c023c77.svg",
   },
@@ -52,76 +59,8 @@ export const products = [
 
   {
     title: "img 4",
-    link: "https://editorially.org",
+    link: "#",
     thumbnail:
       "https://www.ineffa.in/_next/static/media/Image_4.0c023c77.svg",
-  },
-  // {
-  //   title: "Editrix AI",
-  //   link: "https://editrix.ai",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-  // },
-  // {
-  //   title: "Pixel Perfect",
-  //   link: "https://app.pixelperfect.quest",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-  // },
-
-  // {
-  //   title: "Algochurn",
-  //   link: "https://algochurn.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-  // },
-  // {
-  //   title: "Aceternity UI",
-  //   link: "https://ui.aceternity.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-  // },
-  // {
-  //   title: "Tailwind Master Kit",
-  //   link: "https://tailwindmasterkit.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-  // },
-  // {
-  //   title: "SmartBridge",
-  //   link: "https://smartbridgetech.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-  // },
-  // {
-  //   title: "Renderwork Studio",
-  //   link: "https://renderwork.studio",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  // },
-
-  // {
-  //   title: "Creme Digital",
-  //   link: "https://cremedigital.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-  // },
-  // {
-  //   title: "Golden Bells Academy",
-  //   link: "https://goldenbellsacademy.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-  // },
-  // {
-  //   title: "Invoker Labs",
-  //   link: "https://invoker.lol",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-  // },
-  // {
-  //   title: "E Free Invoice",
-  //   link: "https://efreeinvoice.com",
-  //   thumbnail:
-  //     "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-  // },
-];
+  },];
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client";
+
 import React from 'react'
 import {HeroParallaxDemo} from './components/HeroParallaxDemo'
-import { BrowserRouter as Router } from 'react-router-dom';
+
 const page = () => {
   return (
     <div>
 
-      <Router>
+      
       <HeroParallaxDemo/>
-      </Router>
     </div>
   )
 }
