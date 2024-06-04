8fda7f0b46022731c5d227c254f56ba22d8e06eb
Anmol <anmolawasthi117@gmail.com>
2024-06-04 16:51:24 +0530

    added some more components

---
+++ src/app/components/FloatingNavDemo.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "./ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Work",
+      link: "/",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Services",
+      link: "/",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Contact Us",
+      link: "/",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  return (
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 
 import React from 'react'
 import {HeroParallaxDemo} from './components/HeroParallaxDemo'
+import { WobbleCardDemo } from './components/WobbleCardDemo'
+import { FloatingNavDemo } from './components/FloatingNavDemo'
 
+import Footer from './components/Footer'
 const page = () => {
   return (
     <div>
 
-      
+      <FloatingNavDemo/>
       <HeroParallaxDemo/>
+      <WobbleCardDemo/>
+     
+      <Footer/>
+      
     </div>
   )
 }
